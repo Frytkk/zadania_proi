8d31d477ebbe5d8d39e4ba54ecb791acaf838b08
Frytek <116685535+Frytkk@users.noreply.github.com>
2023-05-17 20:23:36 +0200

    Delete specialization_not_found_exception.cpp

--- specialization_not_found_exception.cpp
+++
@@ -1,4 +0,0 @@
-#include "specialization_not_found_exception.h"
-
-SpecializationNotFoundException::SpecializationNotFoundException(std::string specialization):
-    std::invalid_argument("Specialization having the following name not found: " + specialization){}
